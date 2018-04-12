bb869fe683c9e60c0a2f12bf4eeaa4eab87eb19a
SharonLK <sharonlevenzonkuninin@gmail.com>
2018-04-12 15:43:52 +0300

    Print sound files data into the respective 'text' files

--- coldify/pre/organizer.py
+++ coldify/pre/organizer.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from coldify.utils import numeric
 
 
@@ -8,7 +10,7 @@ def extract_words(name):
 
         dan_897.wav
 
-    this function will extract the numbers and return list("eight", "nine", "seven").
+    this function will extract the numbers and return ["eight", "nine", "seven"].
 
     :param name: file name
     :return: list of the words spoken in that .wav file
@@ -17,5 +19,42 @@ def extract_words(name):
     return [numeric.to_string_repr(num) for num in nums]
 
 
+def convert_filename(name):
+    """Converts a file name to the wanted format in the train/test text file
+
+    For example, if the file name is
+
+        chen432.wav
+
+    this function will return the following string:
+
+        chen432 four three two
+
+    :param name: file name
+    :return: representation of this file as needed by the text file
+    """
+    prefix = name[:name.index(".")]
+    return "{} {}".format(prefix, " ".join(extract_words(name)))
+
+
 if __name__ == "__main__":
-    print(extract_words("chen178.wav"))
+    source_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+    types = ["train", "test"]
+
+    for dir_type in types:
+        path = os.path.join(source_dir, "..", "..", "digits_audio", dir_type)
+
+        lines = []
+
+        for name_dir in os.listdir(path):
+            dir_path = os.path.join(path, name_dir)
+            if os.path.isdir(dir_path):
+                for recording in os.listdir(dir_path):
+                    lines.append(convert_filename(recording))
+
+        lines.sort()
+
+        with open(os.path.join(source_dir, "..", "..", "data", dir_type, "text"), "w") as f:
+            for line in lines:
+                f.write("{}{}".format(line, os.linesep))
