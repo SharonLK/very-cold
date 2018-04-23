af3ab01caf307332a1b8f115abb16aeb6bcf7051
Sharon <sharonlevenzonkuninin@gmail.com>
2018-04-23 16:22:56 +0300

    Add option to also record Aaver Shidur sentences
    Currently both recording options use the same recorder, so the GUI is
    prone to errors in case the user clicks both record buttons before
    stopping the other.

--- coldify/generation/ui/gui_test.py
+++ coldify/generation/ui/gui_test.py
@@ -1,17 +1,20 @@
-import sys
 import os
-from PyQt4 import QtGui
+import random
+import sys
 from random import shuffle
+
+from PyQt4 import QtGui
+
 from coldify.generation.recorder import Recorder
 
 
 class Window(QtGui.QWidget):
     DEFAULT_BUTTON_STYLE = "border-radius: 2px;" + \
-        "font-weight: bold;" + \
-        "font-size: 14px;" + \
-        "border-color: black;" + \
-        "border-width: 1px;" + \
-        "border-style: outset;"
+                           "font-weight: bold;" + \
+                           "font-size: 14px;" + \
+                           "border-color: black;" + \
+                           "border-width: 1px;" + \
+                           "border-style: outset;"
 
     def __init__(self):
         super(Window, self).__init__()
@@ -28,34 +31,55 @@ class Window(QtGui.QWidget):
         grid.setVerticalSpacing(10)
         self.setLayout(grid)
 
-        nameLabel = QtGui.QLabel("Name (in English):")
+        nameLabel = QtGui.QLabel("שם (באנגלית)")
         nameLabel.setStyleSheet("font-weight: bold; font-size: 16px;")
 
         self.name = QtGui.QLineEdit()
         self.name.setFixedWidth(300)
         self.name.setStyleSheet("font-size: 16px;")
 
-        explanationLabel = QtGui.QLabel(
-            "When you are ready, press Start, read the following message from left to right and then hit Stop")
-        nameLabel.setStyleSheet("font-weight: bold; font-size: 16px;")
+        # Aaver Shidur
 
-        self.recordingLabel = QtGui.QLabel()
-        self.recordingLabel.setStyleSheet(
+        aaverShidurExpLabel = QtGui.QLabel(
+            r"כשאת\ה מוכן, לחץ על הכפתור ואמור את המשפט הבא, לאחר מכן לחץ על הכפתור פעם נוספת")
+        aaverShidurExpLabel.setStyleSheet("font-weight: bold; font-size: 16px;")
+
+        self.aaverShidurRecordLabel = QtGui.QLabel()
+        self.aaverShidurRecordLabel.setStyleSheet(
             "font-weight: bold; font-size: 16px; padding: 20px 20px 20px 20px")
-        self.recordingLabel.setText("    ".join(
-            [str(num) for num in self.sequence]))
+        self.aaverShidurNumber = random.randint(0, 24)
+        self.aaverShidurRecordLabel.setText("העבר מודול {} לשידור".format(self.aaverShidurNumber))
 
-        self.record = QtGui.QPushButton("Start Recording")
-        self.record.setStyleSheet(self.DEFAULT_BUTTON_STYLE +
-                                  "background-color: #98B475;")
-        self.record.setFixedHeight(40)
-        self.record.clicked.connect(self.recordClicked)
+        self.aaverShidurButton = QtGui.QPushButton("Start Recording")
+        self.aaverShidurButton.setStyleSheet(self.DEFAULT_BUTTON_STYLE +
+                                             "background-color: #98B475;")
+        self.aaverShidurButton.setFixedHeight(40)
+        self.aaverShidurButton.clicked.connect(self.recordAaverShidurClicked)
+
+        # Number Sequence
+
+        numSeqExpLabel = QtGui.QLabel(
+            r"כשאת\ה מוכן, לחץ על הכפתור ואמור את המפרים משמאל לימין, לאחר מכן לחץ על הכפתור פעם נוספת")
+        numSeqExpLabel.setStyleSheet("font-weight: bold; font-size: 16px;")
+
+        self.numSeqRecordLabel = QtGui.QLabel()
+        self.numSeqRecordLabel.setStyleSheet("font-weight: bold; font-size: 16px; padding: 20px 20px 20px 20px")
+        self.numSeqRecordLabel.setText("    ".join([str(num) for num in self.sequence]))
+
+        self.numSeqButton = QtGui.QPushButton("Start Recording")
+        self.numSeqButton.setStyleSheet(self.DEFAULT_BUTTON_STYLE +
+                                        "background-color: #98B475;")
+        self.numSeqButton.setFixedHeight(40)
+        self.numSeqButton.clicked.connect(self.recordNumberSequenceClicked)
 
         grid.addWidget(nameLabel, 0, 0)
         grid.addWidget(self.name, 1, 0)
-        grid.addWidget(explanationLabel, 2, 0)
-        grid.addWidget(self.recordingLabel, 3, 0)
-        grid.addWidget(self.record, 4, 0)
+        grid.addWidget(aaverShidurExpLabel, 2, 0)
+        grid.addWidget(self.aaverShidurRecordLabel, 3, 0)
+        grid.addWidget(self.aaverShidurButton, 4, 0)
+        grid.addWidget(numSeqExpLabel, 5, 0)
+        grid.addWidget(self.numSeqRecordLabel, 6, 0)
+        grid.addWidget(self.numSeqButton, 7, 0)
 
         self.move(300, 300)
         self.setWindowTitle('Cold Recorder')
@@ -64,20 +88,44 @@ class Window(QtGui.QWidget):
         self.sequence = list(range(0, 25))
         shuffle(self.sequence)
 
-    def recordClicked(self):
-        if "Start" in self.record.text():
-            self.record.setText("Stop Recording")
-            self.record.setStyleSheet(self.DEFAULT_BUTTON_STYLE +
-                                      "background-color: #AB4441;")
+    def recordAaverShidurClicked(self):
+        if "Start" in self.aaverShidurButton.text():
+            self.aaverShidurButton.setText("Stop Recording")
+            self.aaverShidurButton.setStyleSheet(self.DEFAULT_BUTTON_STYLE +
+                                                 "background-color: #AB4441;")
             self.recorder.startRecording()
-        elif "Stop" in self.record.text():
-            self.record.setText("Start Recording")
-            self.record.setStyleSheet(self.DEFAULT_BUTTON_STYLE +
-                                      "background-color: #98B475;")
-            self.recorder.stopRecording(os.path.join(self.dictPath, self.name.text(
-            ) + "-6-" + "-".join([str(num) for num in self.sequence]) + ".wav"))
+        elif "Stop" in self.aaverShidurButton.text():
+            self.aaverShidurButton.setText("Start Recording")
+            self.aaverShidurButton.setStyleSheet(self.DEFAULT_BUTTON_STYLE +
+                                                 "background-color: #98B475;")
+            self.recorder.stopRecording(
+                os.path.join(self.dictPath, self.name.text() + "-1-" + str(self.aaverShidurNumber) + ".wav"))
+            self.aaverShidurNumber = random.randint(0, 24)
+            self.aaverShidurRecordLabel.setText("העבר מודול {} לשידור".format(self.aaverShidurNumber))
+
+    def recordAaverAazanaClicked(self):
+        pass
+
+    def recordAktzaaClicked(self):
+        pass
+
+    def recordGibuiClicked(self):
+        pass
+
+    def recordNumberSequenceClicked(self):
+        if "Start" in self.numSeqButton.text():
+            self.numSeqButton.setText("Stop Recording")
+            self.numSeqButton.setStyleSheet(self.DEFAULT_BUTTON_STYLE +
+                                            "background-color: #AB4441;")
+            self.recorder.startRecording()
+        elif "Stop" in self.numSeqButton.text():
+            self.numSeqButton.setText("Start Recording")
+            self.numSeqButton.setStyleSheet(self.DEFAULT_BUTTON_STYLE +
+                                            "background-color: #98B475;")
+            self.recorder.stopRecording(os.path.join(self.dictPath, self.name.text() + "-6-" + "-".join(
+                [str(num) for num in self.sequence]) + ".wav"))
             self.__generate_sequence()
-            self.recordingLabel.setText("    ".join(
+            self.numSeqRecordLabel.setText("    ".join(
                 [str(num) for num in self.sequence]))
 
 
